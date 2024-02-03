b8d6855d74124fef03fc8b9c2485d7ef0144826f
Poisedboy <beschasnuy96@gmail.com>
2024-02-04 01:58:06 +0200

    added interface to ssesion

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -2,11 +2,18 @@ import AdminSettings from "@/components/AdminSettings";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
+interface IUser {
+  role: string | null;
+}
+interface ISession {
+  user: IUser;
+}
+
 const SettingPage = async () => {
-  const session = await getServerSession(authOptions);
+  const session: ISession | null = await getServerSession(authOptions);
   return (
     <div className="mx-7 sm:mx-12 my-5 flex flex-col justify-center items-center">
-      {session?.user.role === "USER" ? <AdminSettings /> : "USER"}
+      {session?.user?.role === "USER" ? <AdminSettings /> : "USER"}
     </div>
   );
 };
