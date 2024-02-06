868cb63afae3075779cc91cdbc7e3a2dacfc9d9a
Poisedboy <beschasnuy96@gmail.com>
2024-02-06 02:10:13 +0200

    added headers to get method

--- app/api/product-list/route.ts
+++ app/api/product-list/route.ts
@@ -2,13 +2,18 @@ import { db } from "@/prisma/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const products = await db.product.findMany();
-    return NextResponse.json({ data: products });
-  } catch (e) {
-    return NextResponse.json(
-      { message: "Something went wrong!" },
-      { status: 500 }
-    );
-  }
+	try {
+		const products = await db.product.findMany();
+		const responseInit = {
+			headers: {
+				"Cache-Control": "s-maxage=1, stale-while-revalidate",
+			},
+		};
+		return NextResponse.json({ data: products }, responseInit);
+	} catch (e) {
+		return NextResponse.json(
+			{ message: "Something went wrong!" },
+			{ status: 500 }
+		);
+	}
 }
