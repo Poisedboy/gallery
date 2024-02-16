c95aba6bf4265fe5cf0e0eef3d7c4636c7fc5f3d
Poisedboy <beschasnuy96@gmail.com>
2024-02-16 22:30:06 +0200

    added description in profile setting

--- components/AdminSettings.tsx
+++ components/AdminSettings.tsx
@@ -4,30 +4,21 @@ import CreateProduct from "./CreateProduct";
 import { ChangeUserData } from "./user/ChangeUserData";
 
 const AdminSettings = () => {
-	return (
-		<div className="w-full">
-			<Accordion>
-				<AccordionItem
-					key="1"
-					aria-label="Accordion 1"
-					title="Security">
-					<ChangeUserData />
-				</AccordionItem>
-				<AccordionItem
-					key="2"
-					aria-label="Accordion 2"
-					title="Create Product">
-					<CreateProduct />
-				</AccordionItem>
-				<AccordionItem
-					key="3"
-					aria-label="Accordion 3"
-					title="Features">
-					Features
-				</AccordionItem>
-			</Accordion>
-		</div>
-	);
+  return (
+    <div className="w-full">
+      <Accordion>
+        <AccordionItem key="1" aria-label="Accordion 1" title="Profile">
+          <ChangeUserData />
+        </AccordionItem>
+        <AccordionItem key="2" aria-label="Accordion 2" title="Create Product">
+          <CreateProduct />
+        </AccordionItem>
+        <AccordionItem key="3" aria-label="Accordion 3" title="Features">
+          Features
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
 };
 
 export default AdminSettings;
--- components/user/ChangeUserData.tsx
+++ components/user/ChangeUserData.tsx
@@ -8,8 +8,9 @@ import {
 } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@nextui-org/react";
+import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
 import { signOut, useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import * as z from "zod";
@@ -17,6 +18,10 @@ import * as z from "zod";
 const FormUserSchema = z.object({
   username: z.string().min(1, "Username is required"),
   email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have than 8 chracters"),
 });
 
 export const ChangeUserData = () => {
@@ -27,6 +32,7 @@ export const ChangeUserData = () => {
     defaultValues: {
       username: "",
       email: "",
+      password: "",
     },
   });
 
@@ -39,6 +45,8 @@ export const ChangeUserData = () => {
     form.setValue("email", session?.user.email);
     form.setValue("username", session?.user.username);
   }, []);
+
+  const [showPass, setShowPass] = useState(false);
 
   const onSubmit = async (values: z.infer<typeof FormUserSchema>) => {
     try {
@@ -67,55 +75,97 @@ export const ChangeUserData = () => {
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-wrap items-center justify-center my-5 gap-5 sm:10 w-full"
-      >
-        <div className="flex flex-col gap-[5px] w-full sm:w-[450px]">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    label="Username"
-                    variant="underlined"
-                    // defaultValue={username}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    label="Email"
-                    variant="underlined"
-                    // defaultValue={email}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button
-            variant="outline"
-            className="w-full rounded-none"
-            type="submit"
-          >
-            Change data
-          </Button>
-        </div>
-      </form>
-    </Form>
+    <>
+      <p className="flex items-center flex-wrap justify-center w-full">
+        After changing data you'll be pulled to login page.
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-wrap items-center justify-center my-5 gap-5 sm:10 w-full"
+        >
+          <div className="flex flex-col gap-[5px] w-full sm:w-[450px]">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      label="Username"
+                      variant="underlined"
+                      // defaultValue={username}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input label="Email" variant="underlined" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input label="Email" variant="underlined" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        label="Password"
+                        variant="underlined"
+                        type={showPass ? "text" : "password"}
+                        {...field}
+                      />
+                      {showPass ? (
+                        <EyeOpenIcon
+                          className="absolute right-1 top-6"
+                          onClick={() => setShowPass(!showPass)}
+                        />
+                      ) : (
+                        <EyeClosedIcon
+                          className="absolute right-1 top-6"
+                          onClick={() => setShowPass(!showPass)}
+                        />
+                      )}
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              variant="outline"
+              className="w-full rounded-none"
+              type="submit"
+            >
+              Change data
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
   );
 };
